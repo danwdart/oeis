a84cc718402b51b6674e697343b91e261b934e0c
Dan Dart <git@dandart.co.uk>
2019-05-16 06:45:25 +0100

    fix tests

--- test/integration/index.js
+++ test/integration/index.js
@@ -1,13 +1,13 @@
-import oeis from '../../src';
+import {searchBySequence, searchByID, plot} from '../../src';
 
-oeis.searchBySequence([1,1,2,3,5])
+searchBySequence([1,1,2,3,5])
     .then((text) => console.log('Sequence', (text.greeting ? 'Success' : 'Failure')));
 
-oeis.searchByID(42)
+searchByID(42)
     .then((text) => console.log('ID', (text.greeting ? 'Success' : 'Failure')));
 
-oeis.plot(42, false)
+plot(42, false)
     .then((text) => console.log('Plot HTML', (text.includes('<html>') ? 'Success' : 'Failure')));
 
-oeis.plot(42, true)
+plot(42, true)
     .then((text) => console.log('Plot PNG', (text.includes('PNG') ? 'Success' : 'Failure')));
