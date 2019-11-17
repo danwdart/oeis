615fecd37f77539ea90a642326882d70a0eb7600
Dan Dart <git@dandart.co.uk>
2019-11-17 23:50:02 +0000

    add getSequence

--- src/index.js
+++ src/index.js
@@ -9,3 +9,5 @@ export const searchByID = intID => fetch(`${ENDPOINT}/search?fmt=json&q=id:A${St
     .then(res => res.json());
 export const plot = (intID, bPng) => fetch(`${ENDPOINT}/A${String(intID).padStart(SIZE, '0')}/graph?png=${(bPng ? '1' : '0')}`)
     .then(res => res.text());
+export const getSequence = intId => fetch(`${ENDPOINT}/A${String(intID)}/b${String(intID)}.txt`)
+    .then(res => res.text());
